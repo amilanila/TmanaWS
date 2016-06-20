f9eb327b6f336e3c3f43639acfd793d72695ad8d
amilanila@gmail.com <amilanila@gmail.com>
2016-06-20 23:17:04 +1000

    Tmana REST web services

---
+++ server/models/facets.js
@@ -0,0 +1,14 @@
+// our example model is just an Array
+var response = {
+	'success': true,
+	'data' : { 
+		'facets' : [
+			{'name': 'div1', 'title': 'Division 1'},
+			{'name': 'div2', 'title': 'Division 2'},
+			{'name': 'div3', 'title': 'Division 3'},
+			{'name': 'div4', 'title': 'Division 4'}
+		]
+	}
+};
+
+export default response;
