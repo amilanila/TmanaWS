c6525af3fc1fc55e6a800c24472125b5658a361a
amilanila@gmail.com <amilanila@gmail.com>
2016-08-25 18:04:32 +1000

    adding senior players

--- server/models/player.js
+++ server/models/player.js
@@ -2,7 +2,10 @@ export const players =  [
 	{'id': 'p1', 'fname': 'Thisal', 'lname': 'Wijekoon', 'category': 'J'},
 	{'id': 'p2', 'fname': 'Hiruki', 'lname': 'Wijekoon', 'category': 'J'},
 	{'id': 'p3', 'fname': 'Ryan', 'lname': 'Premarathna', 'category': 'J'},
-	{'id': 'p4', 'fname': 'Enuri', 'lname': 'Weerarathna', 'category': 'J'}
+	{'id': 'p4', 'fname': 'Enuri', 'lname': 'Weerarathna', 'category': 'J'},
+	{'id': 'p5', 'fname': 'Amila', 'lname': 'Nilantha', 'category': 'S'},
+	{'id': 'p6', 'fname': 'Nishantha', 'lname': 'Premarathna', 'category': 'S'},
+	{'id': 'p7', 'fname': 'Ranjith', 'lname': 'Wanasinghe', 'category': 'S'}
 ];
 
 export const playerAll = () => {
